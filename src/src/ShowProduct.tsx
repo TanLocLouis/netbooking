e4766e3e3b91b662924ad6cbd39f3a07abe4181b
TanLocLouis <ltloc05lumia520@gmail.com>
2025-09-26 22:20:32 +0700

    add to cart

--- src/src/ShowProduct.tsx
+++ src/src/ShowProduct.tsx
@@ -1,6 +1,7 @@
 import './css/ShowProduct.css'
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import Navbar from './Navbar';
 
 function showProduct() {
     const {product_id} = useParams();
@@ -13,6 +14,21 @@ function showProduct() {
         media_url: ""
     }]);
 
+    const handleAddToCart = () => {
+        let cart = localStorage.getItem("cart");
+        let cartItems = cart ? JSON.parse(cart) : [];
+
+        const existing = cartItems.find(item => item.product_id === Product.id);
+
+        if (existing) {
+            existing.quantity += 1;
+        } else {
+            cartItems.push({ product_id: Product.id, quantity: 1 });
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cartItems));
+    }
+
     useEffect(() => {
         const fetchProduct = async () => {
             const response = await fetch("http://localhost:8080/api/v1/product/" + product_id);
@@ -24,6 +40,9 @@ function showProduct() {
 
     return (
         <>
+            <Navbar/>
+
+            <div style={{marginTop: "100px"}}></div>
             <div className="show-product-grid">
                 <div className="show-product-photo">
                     <img className='show-product-photo-canvas' src={Product.media_url}></img>
@@ -31,9 +50,14 @@ function showProduct() {
 
                 <div className="show-product-description">
                     <h1>{Product.name}</h1>
-                    <p>{Product.description}</p>
-                    <h2>Price: {Product.price}</h2>
+                    <p style={{marginBottom: 0}}>{Product.description}</p>
+                    <h2 style={{marginTop: 0}}>Price: {Product.price}</h2>
+
+                    <div>
+                        <button onClick={() => handleAddToCart(Product.id)} className='add-to-cart-button'>Add to Cart</button>
+                    </div>
                 </div>
+
             </div>
         </>
     )
