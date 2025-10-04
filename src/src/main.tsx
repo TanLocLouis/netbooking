6dfead912c23f5a4e41232e5a06a70618dc02d8f
TanLocLouis <ltloc05lumia520@gmail.com>
2025-10-04 22:36:58 +0700

    convert colors to variables

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
+import './css/color.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
